0a90d67f95ec6b2257c447b4ae3aae4fec7fd95f
Marcel Klehr <mklehr@gmx.net>
2026-02-10 11:06:15 +0100

    fix(calendar): Make calendar tools non-blocking
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- ex_app/lib/all_tools/calendar.py
+++ ex_app/lib/all_tools/calendar.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
 # SPDX-License-Identifier: AGPL-3.0-or-later
 import asyncio
+import time
 from datetime import datetime, timezone, timedelta
 from typing import Optional
 
@@ -22,6 +23,11 @@ async def get_tools(nc: AsyncNextcloudApp):
 	ncSync = NextcloudApp()
 	ncSync.set_user(await nc.user)
 
+	def list_calendars_sync():
+		principal = ncSync.cal.principal()
+		calendars = principal.calendars()
+		return ", ".join([cal.name for cal in calendars])
+
 	@tool
 	@safe_tool
 	async def list_calendars():
@@ -29,28 +35,9 @@ async def get_tools(nc: AsyncNextcloudApp):
 		List all existing calendars by name
 		:return: a comma-separated list of calendar names
 		"""
-		principal = ncSync.cal.principal()
-		calendars = principal.calendars()
-		return ", ".join([cal.name for cal in calendars])
+		return await asyncio.to_thread(list_calendars_sync)
 
-	@tool
-	@dangerous_tool
-	async def schedule_event(calendar_name: str, title: str, description: str, start_date: str, end_date: str, attendees: Optional[list[str]], start_time: Optional[str], end_time: Optional[str], location: Optional[str], timezone: Optional[str]):
-		"""
-		Crete a new event or meeting in a calendar. Omit start_time and end_time parameters to create an all-day event.
-		:param calendar_name: The name of the calendar to add the event to
-		:param title: The title of the event
-		:param description: The description of the event
-		:param start_date: the start date of the event in the following form: YYYY-MM-DD e.g. '2024-12-01'
-		:param end_date: the end date of the event in the following form: YYYY-MM-DD e.g. '2024-12-01'
-		:param attendees: the list of attendees to add to the event (as email addresses)
-		:param start_time: the start time in the following form: HH:MM AM/PM e.g. '3:00 PM'
-		:param end_time: the start time in the following form: HH:MM AM/PM e.g. '4:00 PM'
-		:param location: The location of the event
-		:param timezone: Timezone (e.g., 'America/New_York').
-		:return: bool
-		"""
-
+	def schedule_event_sync(calendar_name: str, title: str, description: str, start_date: str, end_date: str, attendees: Optional[list[str]], start_time: Optional[str], end_time: Optional[str], location: Optional[str], timezone: Optional[str]):
 		# Parse date and times
 		start_date = datetime.strptime(start_date, "%Y-%m-%d")
 		end_date = datetime.strptime(end_date, "%Y-%m-%d")
@@ -90,15 +77,15 @@ async def get_tools(nc: AsyncNextcloudApp):
 		i = 0
 		while i < 20:
 			try:
-				json = await nc.ocs('GET', '/ocs/v2.php/cloud/user')
+				json = ncSync.ocs('GET', '/ocs/v2.php/cloud/user')
 				break
 			except (
 					ConnectionError,
 					Timeout
 			) as e:
-				await log(nc, LogLvl.DEBUG, "Ignored error during task polling")
+				log(ncSync, LogLvl.DEBUG, "Ignored error during task polling")
 				i += 1
-				await asyncio.sleep(1)
+				time.sleep(1)
 				continue
 
 		# ...and set the organizer
@@ -112,8 +99,89 @@ async def get_tools(nc: AsyncNextcloudApp):
 		calendar = {cal.name: cal for cal in calendars}[calendar_name]
 		calendar.add_event(str(c))
 
+	@tool
+	@dangerous_tool
+	async def schedule_event(calendar_name: str, title: str, description: str, start_date: str, end_date: str, attendees: Optional[list[str]], start_time: Optional[str], end_time: Optional[str], location: Optional[str], timezone: Optional[str]):
+		"""
+		Crete a new event or meeting in a calendar. Omit start_time and end_time parameters to create an all-day event.
+		:param calendar_name: The name of the calendar to add the event to
+		:param title: The title of the event
+		:param description: The description of the event
+		:param start_date: the start date of the event in the following form: YYYY-MM-DD e.g. '2024-12-01'
+		:param end_date: the end date of the event in the following form: YYYY-MM-DD e.g. '2024-12-01'
+		:param attendees: the list of attendees to add to the event (as email addresses)
+		:param start_time: the start time in the following form: HH:MM AM/PM e.g. '3:00 PM'
+		:param end_time: the start time in the following form: HH:MM AM/PM e.g. '4:00 PM'
+		:param location: The location of the event
+		:param timezone: Timezone (e.g., 'America/New_York').
+		:return: bool
+		"""
+		await asyncio.to_thread(schedule_event_sync, calendar_name, title, description, start_date, end_date, attendees, start_time, end_time, location, timezone)
+
 		return True
 
+	def find_free_time_slot_in_calendar_sync(participants: list[str], slot_duration: Optional[float],
+												  start_time: Optional[str], end_time: Optional[str]):
+			me = ncSync.ocs('GET', '/ocs/v2.php/cloud/user')
+
+			attendees = 'ORGANIZER:mailto:' + me['email'] + '\n'
+			attendees += 'ATTENDEE:mailto:' + me['email'] + '\n'
+			for attendee in participants:
+				attendees += f"ATTENDEE:mailto:{attendee}\n"
+
+			if start_time is None:
+				start_time = round_to_nearest_half_hour(datetime.now(timezone.utc))
+			else:
+				start_time = datetime.combine(datetime.strptime(start_time, "%Y-%m-%d").date(), datetime.min.time(),
+											  timezone.utc)
+			if end_time is None:
+				end_time = start_time + timedelta(days=7)
+			else:
+				end_time = datetime.combine(datetime.strptime(end_time, "%Y-%m-%d").date(), datetime.min.time(),
+											timezone.utc)
+				if start_time >= end_time:
+					end_time = start_time + timedelta(days=7)
+
+			dtstart = start_time.strftime("%Y%m%dT%H%M%SZ")
+			dtend = end_time.strftime("%Y%m%dT%H%M%SZ")
+
+			freebusyRequest = """
+	BEGIN:VCALENDAR
+	PRODID:-//IDN nextcloud.com//Calendar app 5.1.0-beta.2//EN
+	CALSCALE:GREGORIAN
+	VERSION:2.0
+	METHOD:REQUEST
+	BEGIN:VFREEBUSY
+	DTSTAMP:20250131T123029Z
+	UID:03c8f220-d313-4c86-ae06-19fbae157079
+	DTSTART:{DTSTART}
+	DTEND:{DTEND}
+	{ATTENDEES}END:VFREEBUSY
+	END:VCALENDAR
+	""".replace('{ATTENDEES}', attendees).replace('{DTSTART}', dtstart).replace('{DTEND}', dtend)
+			username = ncSync._session.user
+			response = ncSync._session._create_adapter(True).request('POST',
+																	   f"{nc.app_cfg.endpoint}/remote.php/dav/calendars/{username}/outbox/",
+																	   headers={
+																		   "Content-Type": "text/calendar; charset=utf-8",
+																		   "Depth": "0",
+																	   }, content=freebusyRequest)
+			print(freebusyRequest)
+			print(response.text)
+
+			# Parse the XML response to extract vCard data
+			namespace = {"CAL": "urn:ietf:params:xml:ns:caldav"}  # Define the namespace
+			root = ET.fromstring(response.text)
+			vcal_elements = root.findall(".//CAL:calendar-data", namespace)
+			# Parse vcal strings into dictionaries
+			busy_times = []
+			for vcal_element in vcal_elements:
+				vcal_text = vcal_element.text.strip()
+				vcal = vobject.readOne(vcal_text)
+				for fb in vcal.vfreebusy.contents.get("freebusy", []):
+					busy_times.append(fb.value[0])
+			available_slots = find_available_slots(start_time, end_time, busy_times, timedelta(hours=slot_duration))
+			return available_slots
 
 	@tool
 	@safe_tool
@@ -126,79 +194,11 @@ async def get_tools(nc: AsyncNextcloudApp):
 		:param end_time: the end time of the range within which to check for free slots (by default this will be 7 days after start_time; use the following format: 2025-01-31)
 		:return:
 		"""
-
-		me = await nc.ocs('GET', '/ocs/v2.php/cloud/user')
-
-		attendees = 'ORGANIZER:mailto:'+me['email']+'\n'
-		attendees += 'ATTENDEE:mailto:'+me['email']+'\n'
-		for attendee in participants:
-			attendees += f"ATTENDEE:mailto:{attendee}\n"
-
-		if start_time is None:
-			start_time = round_to_nearest_half_hour(datetime.now(timezone.utc))
-		else:
-			start_time = datetime.combine(datetime.strptime(start_time, "%Y-%m-%d").date(), datetime.min.time(), timezone.utc)
-		if end_time is None:
-			end_time = start_time + timedelta(days=7)
-		else:
-			end_time = datetime.combine(datetime.strptime(end_time, "%Y-%m-%d").date(), datetime.min.time(), timezone.utc)
-			if start_time >= end_time:
-				end_time = start_time + timedelta(days=7)
-
-		dtstart = start_time.strftime("%Y%m%dT%H%M%SZ")
-		dtend = end_time.strftime("%Y%m%dT%H%M%SZ")
-
-		freebusyRequest = """
-BEGIN:VCALENDAR
-PRODID:-//IDN nextcloud.com//Calendar app 5.1.0-beta.2//EN
-CALSCALE:GREGORIAN
-VERSION:2.0
-METHOD:REQUEST
-BEGIN:VFREEBUSY
-DTSTAMP:20250131T123029Z
-UID:03c8f220-d313-4c86-ae06-19fbae157079
-DTSTART:{DTSTART}
-DTEND:{DTEND}
-{ATTENDEES}END:VFREEBUSY
-END:VCALENDAR
-""".replace('{ATTENDEES}', attendees).replace('{DTSTART}', dtstart).replace('{DTEND}', dtend)
-		username = nc._session.user
-		response = await nc._session._create_adapter(True).request('POST', f"{nc.app_cfg.endpoint}/remote.php/dav/calendars/{username}/outbox/", headers={
-			"Content-Type": "text/calendar; charset=utf-8",
-			"Depth": "0",
-		}, content=freebusyRequest)
-		print(freebusyRequest)
-		print(response.text)
-
-		# Parse the XML response to extract vCard data
-		namespace = {"CAL": "urn:ietf:params:xml:ns:caldav"}  # Define the namespace
-		root = ET.fromstring(response.text)
-		vcal_elements = root.findall(".//CAL:calendar-data", namespace)
-		# Parse vcal strings into dictionaries
-		busy_times = []
-		for vcal_element in vcal_elements:
-			vcal_text = vcal_element.text.strip()
-			vcal = vobject.readOne(vcal_text)
-			for fb in vcal.vfreebusy.contents.get("freebusy", []):
-				busy_times.append(fb.value[0])
-		available_slots = find_available_slots(start_time, end_time, busy_times, timedelta(hours=slot_duration))
+		available_slots = asyncio.to_thread(find_free_time_slot_in_calendar_sync, participants, slot_duration, start_time, end_time)
 		return available_slots
 
-
-	@tool
-	@dangerous_tool
-	async def add_task(calendar_name: str, title: str, description: str, due_date: Optional[str], due_time: Optional[str], timezone: Optional[str],):
-		"""
-		Crete a new task in a calendar.
-		:param calendar_name: The name of the calendar to add the task to
-		:param title: The title of the task
-		:param description: The description of the task
-		:param due_date: the due date of the event in the following form: YYYY-MM-DD e.g. '2024-12-01'
-		:param due_time: the due time in the following form: HH:MM AM/PM e.g. '3:00 PM'
-		:param timezone: Timezone (e.g., 'America/New_York'). Is required if there is a specified due date.
-		:return: bool
-		"""
-
+	def add_task_sync(calendar_name: str, title: str, description: str, due_date: Optional[str],
+					   due_time: Optional[str], timezone: Optional[str]):
 		description_with_ai_note = f"{description}\n\n---\n\nThis task was scheduled by Nextcloud AI Assistant."
 
 		# Create task
@@ -225,7 +225,7 @@ END:VCALENDAR
 				due_datetime = tz.localize(due_datetime)
 
 			t.due = due_datetime
-		
+
 		# Add event to calendar
 		c.todos.add(t)
 
@@ -233,6 +233,24 @@ END:VCALENDAR
 		calendars = principal.calendars()
 		calendar = {cal.name: cal for cal in calendars}[calendar_name]
 		calendar.add_todo(t.serialize())
+
+		return True
+
+	@tool
+	@dangerous_tool
+	async def add_task(calendar_name: str, title: str, description: str, due_date: Optional[str], due_time: Optional[str], timezone: Optional[str]):
+		"""
+		Crete a new task in a calendar.
+		:param calendar_name: The name of the calendar to add the task to
+		:param title: The title of the task
+		:param description: The description of the task
+		:param due_date: the due date of the event in the following form: YYYY-MM-DD e.g. '2024-12-01'
+		:param due_time: the due time in the following form: HH:MM AM/PM e.g. '3:00 PM'
+		:param timezone: Timezone (e.g., 'America/New_York'). Is required if there is a specified due date.
+		:return: bool
+		"""
+
+		asyncio.to_thread(add_task_sync, calendar_name, title, description, due_date, due_time, timezone)
 
 		return True
 
