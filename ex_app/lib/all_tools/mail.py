ec46c473eecd8916d2232f480a94d437e53c9bd4
Marcel Klehr <mklehr@gmx.net>
2026-03-23 10:49:50 +0100

    fix(mail): remove useless changes
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- ex_app/lib/all_tools/mail.py
+++ ex_app/lib/all_tools/mail.py
@@ -53,92 +53,11 @@ async def get_tools(nc: AsyncNextcloudApp):
 		"""
 
 		return await nc.ocs('GET', '/ocs/v2.php/apps/mail/account/list')
-
-
-	@tool
-	@safe_tool
-	async def list_mail_folders(account_id: int):
-		"""
-		List all mail folders/mailboxes for an account
-		:param account_id: The id of the account (obtainable via get_mail_account_list)
-		:return: list of folders with their ids, names, and message counts
-		"""
-		response = await nc._session._create_adapter(True).request('GET', f"{nc.app_cfg.endpoint}/index.php/apps/mail/api/mailboxes", headers={
-			"Content-Type": "application/json",
-			"OCS-APIREQUEST": "true",
-		}, params={'accountId': account_id})
-		return response.json()
-
-	@tool
-	@safe_tool
-	async def get_email_messages(mailbox_id: int, limit: int = 20):
-		"""
-		Get messages from a specific mailbox
-		:param mailbox_id: The id of the mailbox (obtainable via list_mail_folders)
-		:param limit: Maximum number of messages to return (default 20)
-		:return: list of email messages
-		"""
-		response = await nc._session._create_adapter(True).request('GET', f"{nc.app_cfg.endpoint}/index.php/apps/mail/api/messages", headers={
-			"Content-Type": "application/json",
-			"OCS-APIREQUEST": "true",
-		}, params={'mailboxId': mailbox_id, 'limit': limit})
-		return response.json()
-
-	@tool
-	@safe_tool
-	async def search_emails(mailbox_id: int, search_term: str, limit: int = 20):
-		"""
-		Search for emails in a mailbox
-		:param mailbox_id: The id of the mailbox to search in (obtainable via list_mail_folders)
-		:param search_term: The text to search for in emails
-		:param limit: Maximum number of results to return (default 20)
-		:return: list of matching emails
-		"""
-		response = await nc._session._create_adapter(True).request('GET', f"{nc.app_cfg.endpoint}/index.php/apps/mail/api/messages", headers={
-			"Content-Type": "application/json",
-			"OCS-APIREQUEST": "true",
-		}, params={'mailboxId': mailbox_id, 'filter': search_term, 'limit': limit})
-		return response.json()
-
-	@tool
-	@dangerous_tool
-	async def move_email_to_folder(message_id: int, dest_mailbox_id: int):
-		"""
-		Move an email to a different folder
-		:param message_id: The id of the message to move (obtainable via get_email_messages or search_emails)
-		:param dest_mailbox_id: The id of the destination mailbox (obtainable via list_mail_folders)
-		:return: confirmation
-		"""
-		response = await nc._session._create_adapter(True).request('POST', f"{nc.app_cfg.endpoint}/index.php/apps/mail/api/messages/{message_id}/move", headers={
-			"Content-Type": "application/json",
-			"OCS-APIREQUEST": "true",
-		}, json={
-			'destFolderId': dest_mailbox_id
-		})
-		return response.json()
-
-	@tool
-	@dangerous_tool
-	async def delete_email(message_id: int):
-		"""
-		Delete an email message
-		:param message_id: The id of the message to delete (obtainable via get_email_messages or search_emails)
-		:return: confirmation
-		"""
-		response = await nc._session._create_adapter(True).request('DELETE', f"{nc.app_cfg.endpoint}/index.php/apps/mail/api/messages/{message_id}", headers={
-			"Content-Type": "application/json",
-			"OCS-APIREQUEST": "true",
-		})
-		return response.json()
+		
 
 	return [
 		send_email,
-		get_mail_account_list,
-		list_mail_folders,
-		search_emails,
-		get_email_messages,
-		move_email_to_folder,
-		delete_email
+		get_mail_account_list
 	]
 
 def get_category_name():
