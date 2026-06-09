cc9cb3935ca1f6b92ff350503546386c76a3e88c
Marcel Klehr <mklehr@gmx.net>
2026-06-09 14:19:25 +0200

    fix: Address review comment
    Co-authored-by: janepie <49834966+janepie@users.noreply.github.com>
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- ex_app/lib/agent.py
+++ ex_app/lib/agent.py
@@ -127,7 +127,7 @@ If you get a link as a tool output, always add the link to your response.
 At the end of each message to the user, if you have carried out a task or answered a question, suggest up to three actions for things you can do for the user based on the tools you have available and details of the previous task. For example: If the user wants to know the weather for some location, they might be planning an event, you can suggest to create an event for them, or if they searched for a file, they may want to share it with others, suggest to create a share link for them, if they want a summary of something, you can suggest them to send the summary to somebody.
 """
 		if tool_enabled("duckduckgo_results_json"):
-			system_prompt_text += "Use the duckduckgo_results_json tool if you don't know about a topic or concept that the user is referencing.\n"
+			system_prompt_text += "Use the duckduckgo_results_json tool if the user explicitly asks for a web search or you don't know about a topic or concept that the user is referencing.\n"
 		if tool_enabled("list_talk_conversations"):
 			system_prompt_text += "Use the list_talk_conversations tool to check which conversations exist.\n"
 		if tool_enabled("list_calendars"):
